0fee0363ab1ffabb6c932c8917fff169c5563387
AlucardSanin <alucardsanin@gmail.com>
2022-08-25 23:16:39 -0400

    Display API response in a list

--- src/APIset.js
+++ src/APIset.js
@@ -1,17 +1,17 @@
 /* eslint-disable import/prefer-default-export */
-export const MarcoPolo = {
-  async addScore(user, score) {
-    const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/94MD1RzkEJ6LkZ0NpORz/scores/';
-    const response = await fetch(idUrl, {
-      method: 'POST',
-      body: JSON.stringify({
-        "user": user,
-        "score": score,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    console.log(response.json());
-  },
-};
+export class MarcoPolo {
+ static addScore = async (user, score) => {
+   const idUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4nTGw7Pv17Te8sjkTexb/scores/';
+   const response = await fetch(idUrl, {
+     method: 'POST',
+     body: JSON.stringify({
+       user,
+       score,
+     }),
+     headers: {
+       'Content-type': 'application/json; charset=UTF-8',
+     },
+   });
+   return response;
+ }
+}
